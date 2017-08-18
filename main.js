ea20fe3f3e66298ee38ddf43be469a4903a3bbaa
David Dassau <todieahero@hotmail.com>
2017-08-17 21:11:41 -0500

    wrote JS to print new card with delete button to dom

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
-// Meme Array
-var memeArray = [{
-	name: 'Why are you like this?',
-	img: 'img/one.jpg'
-},  {
-	name: 'Philosoraptor',
-	img: 'img/two.jpg'
-}];
 
-// Variable to get main 'memeContainer' div
-var memeContainerDiv = document.getElementById('memeContainer');
+// Variable to get main 'mainContainer' div
+var mainContainerDiv = document.getElementById('mainContainer');
+// 
+var createBTN = document.getElementById('createButton');
+
+
+function domString() {
+	var userInputText = document.getElementById('inputText').value;
+	var card = '';
+		card += `<div id="mainCard">`;
+		card += `<div class="userInput">${userInputText}</div>`;
+		card += `<div><button id="clearButton">Delete</button></div>`
+		card += `</div>`;
+
+	writeToDom(card);
+}
+
+
+// Function to write content to page
+function writeToDom(stringToDom){
+	mainContainerDiv.innerHTML = stringToDom;
+}
+
+// When button is clicked, content is displayed on page
+createButton.addEventListener('click', function(){
+	domString(mainContainerDiv);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
